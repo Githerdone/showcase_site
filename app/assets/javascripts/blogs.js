e26fbc2fe0013844d6d9041c2dea509735318e81
Githerdone <GitHub@JakeDanforth.com>
2013-11-01 12:35:06 -0700

    adding flatui js file to assests

--- app/assets/javascripts/blogs.js
+++ app/assets/javascripts/blogs.js
@@ -1,2 +1,32 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+var toggleHandler = function(toggle) {
+    var toggle = toggle;
+    var radio = $(toggle).find("input");
+
+    var checkToggleState = function() {
+        if (radio.eq(0).is(":checked")) {
+            $(toggle).removeClass("toggle-off");
+        } else {
+            $(toggle).addClass("toggle-off");
+        }
+    };
+
+    checkToggleState();
+
+    radio.eq(0).click(function() {
+        $(toggle).toggleClass("toggle-off");
+    });
+
+    radio.eq(1).click(function() {
+        $(toggle).toggleClass("toggle-off");
+    });
+};
+
+$(document).ready(function() {
+    $(".toggle").each(function(index, toggle) {
+        toggleHandler(toggle);
+    });
+});
