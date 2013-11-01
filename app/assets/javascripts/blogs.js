e1e429a83c136f5426403d566b4a4a50ff0e0dfa
Githerdone <GitHub@JakeDanforth.com>
2013-11-01 16:24:48 -0700

    testing flatui

--- app/assets/javascripts/blogs.js
+++ app/assets/javascripts/blogs.js
@@ -1,32 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-
-var toggleHandler = function(toggle) {
-    var toggle = toggle;
-    var radio = $(toggle).find("input");
-
-    var checkToggleState = function() {
-        if (radio.eq(0).is(":checked")) {
-            $(toggle).removeClass("toggle-off");
-        } else {
-            $(toggle).addClass("toggle-off");
-        }
-    };
-
-    checkToggleState();
-
-    radio.eq(0).click(function() {
-        $(toggle).toggleClass("toggle-off");
-    });
-
-    radio.eq(1).click(function() {
-        $(toggle).toggleClass("toggle-off");
-    });
-};
-
-$(document).ready(function() {
-    $(".toggle").each(function(index, toggle) {
-        toggleHandler(toggle);
-    });
-});
+//= require jquery.dropkick
+//= require custom_checkbox_and_radio
+//= require custom_radio
+//= require jquery.tagsinput
+//= require bootstrap-tooltip
+//= require jquery.placeholder
+//= require run_everything
